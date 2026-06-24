795c819404672cb68be42ab9ed0c0e654e4d92fe
Muamar-jtm <alimuamarj11@gmail.com>
2026-06-24 09:28:55 +0700

    Endpoint POST api/chat

--- index.js
+++ index.js
@@ -12,3 +12,26 @@ app.use(cors());
 app.use(express.json());
 const PORT = 3000;
 app.listen(PORT, ()=> console.log(`Server ready on http://localhost:${PORT}`));
+
+app.post('/api/chat', async (req, res) =>{
+    const { conversation } = req.body;
+    try {
+        if (!Array.isArray(conversation)) throw new Error('Messages must be an array!');
+        const contents = conversation.map(({ role, text }) =>({
+            role,
+            parts: [{ text }]
+        }));
+
+        const response = await ai.models.generateContent({
+            model: GEMINI_MODEL,
+            contents,
+            config: {
+                temperature: 0.9,
+                systemInstruction: "Jawab hanya menggunakan bahasa Indonesia.",
+            },
+        });
+        res.status(200).json({ result: response.text });
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+});
